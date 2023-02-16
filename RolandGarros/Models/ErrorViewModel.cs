fecf1495aab6d1b37d5a05501c0c1cc23a2f3947
pemace <pe.mace@gmail.com>
2023-02-16 09:56:16 +0100

    Ajoutez des fichiers projet.

---
+++ RolandGarros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RolandGarros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
