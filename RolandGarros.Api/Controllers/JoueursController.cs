5da593da94791c9a920bef3af37b9d37b24addd1
pemace <pe.mace@gmail.com>
2023-02-21 21:37:35 +0100

    APi

---
+++ RolandGarros.Api/Controllers/JoueursController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RolandGarros.Data;
+using RolandGarros.Entities;
+
+namespace RolandGarros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JoueursController : ControllerBase
+    {
+        private readonly TennisContext _context;
+
+        public JoueursController(TennisContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Joueurs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Joueur>>> GetJoueurs()
+        {
+            return await _context.Joueurs.Include(j=>j.Nationalite).ToListAsync();
+        }
+
+        // GET: api/Joueurs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Joueur>> GetJoueur(int id)
+        {
+            var joueur = await _context.Joueurs.Include(j => j.Nationalite).SingleOrDefaultAsync(j => j.Id == id);
+
+            if (joueur == null)
+            {
+                return NotFound();
+            }
+
+            return joueur;
+        }
+
+        // PUT: api/Joueurs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJoueur(int id, Joueur joueur)
+        {
+            if (id != joueur.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(joueur).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JoueurExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Joueurs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Joueur>> PostJoueur(Joueur joueur)
+        {
+            _context.Joueurs.Add(joueur);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJoueur", new { id = joueur.Id }, joueur);
+        }
+
+        // DELETE: api/Joueurs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJoueur(int id)
+        {
+            var joueur = await _context.Joueurs.FindAsync(id);
+            if (joueur == null)
+            {
+                return NotFound();
+            }
+
+            _context.Joueurs.Remove(joueur);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JoueurExists(int id)
+        {
+            return _context.Joueurs.Any(e => e.Id == id);
+        }
+    }
+}
